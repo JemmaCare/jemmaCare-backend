ea142cfab854ebc8f9c67fc419424ed8ad742d8d
owarewaa <akosuaowarewaa360@gmail.com>
2024-08-16 13:07:47 +0000

    changes

--- models/patient_model.js
+++ models/patient_model.js
@@ -9,20 +9,12 @@ export const countryNames = getNames();
 const patientResponseSchema = new Schema({
 
     therapyType: { type: String, enum: ["Bipolar", "Depression", "Psychosis", "Personality disorders", "Anxiety","Schizophrenia", "Body Dysmorphic Disorder", "Obsessive Compulsive Disorder", "Postpartum Depression"], required: true },
-    age: { 
-        type: [Number], 
-        required: true, 
-        validate: {
-            validator: v => v.length === 2 && v[0] < v[1],
-            message: props => `Age range should consist of two numbers where the first number is less than the second`
-        } 
-    },
+    age: {type: Number},
     nationality: { type: String, enum: countryNames, required: true },
     phone: { type: String },
     address: { type: String },
     gender: { type: String, enum: ["male", "female"] },
     previousTherapy: { type: Boolean, required: true },
-    reason: { type: String, required: true },
     feelings: { type: String, required: true },
     userId: { type: Types.ObjectId, ref: 'User', select: true }
 
--- validators/patient_validator.js
+++ validators/patient_validator.js
@@ -5,24 +5,12 @@ import { countryNames } from "../models/patient_model.js";
 
 export const patientResponseValidator = Joi.object({
     therapyType: Joi.string().valid("Bipolar", "Depression", "Psychosis", "Personality disorders", "Anxiety","Schizophrenia", "Body Dysmorphic Disorder", "Obsessive Compulsive Disorder", "Postpartum Depression").required(),
-    age: { 
-        type: [Number], 
-        required: true, 
-        validate: {
-            validator: function(v) {
-                // Check that the array has two elements and the first number is less than the second
-                return Array.isArray(v) && v.length === 2 && v[0] < v[1];
-            },
-            message: props => `Age range should consist of two numbers where the first number (${props.value[0]}) is less than the second (${props.value[1]}).`
-        }
-    },
+    age: Joi.number(),   
     nationality: Joi.string().valid(...countryNames).required(),
     phone: Joi.string(),
-    languages: Joi.array().items(Joi.string()).required(),
     address: Joi.string(),
     gender: Joi.string().valid('male', 'female'),
     previousTherapy: Joi.boolean().required(),
-    reason: Joi.string().required(),
     feelings: Joi.string().required(),
 })
 
@@ -30,13 +18,11 @@ export const patientResponseValidator = Joi.object({
 
 export const patientUpdateResponseValidator = Joi.object({
     therapyType: Joi.string().valid("Bipolar", "Depression", "Psychosis", "Personality disorders", "Schizophrenia", "Body Dysmorphic Disorder", "Obsessive Compulsive Disorder", "Postpartum Depression").required(),
-    age: Joi.number().optional(),
+    age: Joi.number()  ,
     nationality: Joi.string().valid(...countryNames).optional(),
     phone: Joi.string(),
-    languages: Joi.array().items(Joi.string()),
     address: Joi.string(),
     gender: Joi.string().valid('male', 'female'),
     previousTherapy: Joi.boolean().optional(),
-    reason: Joi.string().optional(),
     feelings: Joi.string().optional(),
 })
